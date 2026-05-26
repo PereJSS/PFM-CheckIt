fc3c4615e73f04b291c4386892f1d0bb93461537
Pere Joan <pere_joan_sancho@hotmail.es>
2026-05-26 18:41:41 +0200

    config: add CORS middleware and allow Vercel origin

--- checkIt/core/settings.py
+++ checkIt/core/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -155,3 +156,7 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'https://pfm-check-it.vercel.app',
+]
