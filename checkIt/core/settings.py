7617dc7fc5758aab2bbafec6a3fd301a27259507
Pere Joan <pere_joan_sancho@hotmail.es>
2026-05-26 19:11:49 +0200

    fix: allow render host and trust vercel origin

--- checkIt/core/settings.py
+++ checkIt/core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='localhost,127.0.0.1,pfm-checkit.onrender.com',
+    cast=Csv(),
+)
 
 
 # Application definition
@@ -158,5 +162,9 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
+    'https://pfm-check-it.vercel.app',
+]
+
+CSRF_TRUSTED_ORIGINS = [
     'https://pfm-check-it.vercel.app',
 ]
